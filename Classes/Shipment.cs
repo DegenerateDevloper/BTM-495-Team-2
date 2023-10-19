6ecace5d97af40a3193d9618cb3ad7d7b6a11194
DegenerateDevloper <118934117+DegenerateDevloper@users.noreply.github.com>
2023-10-18 20:19:15 -0400

    adding attributes/methods

---
+++ Classes/Shipment.cs
@@ -0,0 +1,12 @@
+public class Shipment
+{
+    public int shipment_ID;
+    public decimal shipment_cost;
+    private string shipment_date;
+    private string shipment_status;
+    private string shipment_type;
+    private DeliveryMethod deliveryMethod;
+    private ShoppingCart shoppingCart
+
+
+}
